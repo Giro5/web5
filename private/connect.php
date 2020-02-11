750f7d7c132ceee9ae85b451b8e018ea34e08c6c
Nikita <39918253+Giro5@users.noreply.github.com>
2020-02-12 03:52:50 +0500

    f

--- private/connect.php
+++ private/connect.php
@@ -1,7 +1,7 @@
 <?php
 $addr = "localhost";
 $user = "root";
-$pass = "";
+$password = "";
 $db = "web5";
 $port = "3307";
-$link = mysqli_connect($addr, $user, $pass, $db, $port);
+$link = mysqli_connect($addr, $user, $password, $db, $port);
